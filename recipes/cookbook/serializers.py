3c043e036ebe05bf088f8faeb15559d517d9ec6e
MikolajKubek <mikolajkubik1998@gmail.com>
2020-01-25 17:31:14 +0100

    add react

--- recipes/cookbook/serializers.py
+++ recipes/cookbook/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'login', 'password']
+        fields = ['id', 'login', 'password', 'token']
 
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
